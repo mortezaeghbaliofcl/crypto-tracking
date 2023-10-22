3ff770bdaf6466c6103681794003287318991f98
Morteza Eghbali <morync@gmail.com>
2023-10-22 15:26:21 +0200

    navebar and footer completed

--- src/components/ui/Footer.component.jsx
+++ src/components/ui/Footer.component.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const FooterComponent = () => {
-  return (
-    <div>
-      <h4>FooterComponent</h4>
-    </div>
-  )
+    return (
+        <footer className='footer'>
+            <span>
+                Morteza Crypto Tracking.
+            </span>
+            <span className='d-none d-md-inline ms-2'>
+                All rights reserved
+            </span>
+
+
+        </footer>
+    )
 }
 
 export default FooterComponent
