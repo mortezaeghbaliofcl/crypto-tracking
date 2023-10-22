df40078e34bd9c0801132127fc2d79c3aad70b0b
Morteza Eghbali <morync@gmail.com>
2023-10-22 13:42:50 +0200

    implement base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import FooterComponent from "./components/ui/Footer.component";
+import NavbarComponent from "./components/ui/Navbar.component";
 
 
 function App() {
   return (
-    <div className="App">
-      <h3>Crypto</h3>
+    <div>
+
+      {/* Navbar */}
+      <NavbarComponent/>
+
+
+      {/* wrapper*/}
+      <div className="wrapper">
+        <h2>wrapper</h2>
+      </div>
+
+
+      {/* Footer */}
+      <FooterComponent/>
+
     </div>
   );
 }
---
+++ src/components/ui/Navbar.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavbarComponent = () => {
+  return (
+    <div>
+
+        <h4>NavebarComponent</h4>
+      
+    </div>
+  )
+}
+
+export default NavbarComponent
