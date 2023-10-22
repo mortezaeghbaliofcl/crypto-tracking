915913a3c6cc3eddd3970b6cee17a02170abad6f
Morteza Eghbali <morync@gmail.com>
2023-10-22 14:43:46 +0200

    navbar implemented

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './Navbar.css'
+import { FaCoins } from 'react-icons/fa'
 
 const NavbarComponent = () => {
-  return (
-    <div>
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <FaCoins className='navbar-icon' />
+                    <span className='navbar-logo-text'>
+                        Crypto Tracking
+                    </span>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="/" >Home</Link >
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/faq" >FAQ</Link >
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about-us" >About Us</Link >
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact-us" >Contact Us</Link >
+                        </li>
 
-        <h4>NavebarComponent</h4>
-      
-    </div>
-  )
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default NavbarComponent
