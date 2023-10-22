e8300b569db7a04fd00707ed25af418cb764773c
Morteza Eghbali <morync@gmail.com>
2023-10-22 15:44:19 +0200

    spinner implemented

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import SpinnerComponent from '../components/ui/Spinner.component'
 
 const HomePage = () => {
   return (
     <div>
         <h1>Home Page</h1>
-      
+        <SpinnerComponent/>
+        <h1>Home Page</h1>
+
     </div>
   )
 }
