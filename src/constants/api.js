af58de8b20cfbb58f95e4d7d3c543856975e3775
Morteza Eghbali <morync@gmail.com>
2023-10-23 02:06:48 +0200

    api(s) and docs

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,4 @@
 export const baseURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+export const detailsUrl =(id)=> {
+    return `https://api.coingecko.com/api/v3/coins/${id}`;
+}
